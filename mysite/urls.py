eff186e119bf43dca3f444ad9b4c118333e8fd4f
Aidan <adssoccer1@gmail.com>
2020-01-26 21:05:22 -0500

    Added the first html for the site in the templates folder in the blog app.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 #Remember when we talked about a mail carrier checking where to deliver a letter#urls.py file contains a list of patterns used by urlresolver.
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
 ]
