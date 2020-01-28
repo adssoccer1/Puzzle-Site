d2b340a4f8e3a9405de3501b01b90ec63ca84101
Aidan <adssoccer1@gmail.com>
2020-01-27 20:46:54 -0500

    reset2

--- blog/migrations/0002_auto_20200127_2239.py
+++ blog/migrations/0002_auto_20200127_2239.py
@@ -6,9 +6,7 @@ import markupfield.fields
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('blog', '0001_initial'),
-    ]
+   
 
     operations = [
         migrations.AddField(
